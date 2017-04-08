b9c5adf813845ba123d72522f61b06cd6ab25c56
Reed Jones <develop@reedjones.com>
2017-04-08 10:24:16 -0600

    Switched to  css Github ribbon

--- getJSONHighScores_example.php
+++ getJSONHighScores_example.php
@@ -9,39 +9,41 @@
 		exit;
 	}
 
-	$json = '{"highscores": '."\n";
-	$json .= '{"easy": ['."\n";
+	header('Content-type:application/json;charset=utf-8');
+
+	$json = '{"highscores":';
+	$json .= '{"easy": [';
 	while ($row = pg_fetch_array($result))
 	{
 		
-		$json .= "    " . $row[0] . ",\n"; 
+		$json .= $row[0] . ","; 
 	}
-	$json = trim($json, ",\n");
-	$json .= "\n  ],\n";
+	$json = trim($json, ",");
+	$json .= " ],";
 
 
 	$result = pg_query($con, "SELECT json_build_object('Name', name, 'Time', time) FROM (SELECT name, time FROM _Table_ WHERE difficulty = 1 ORDER BY time, id LIMIT 10) _Table_" );
 
-	$json .= '"medium": ['."\n";
+	$json .= '"medium": [';
 	while ($row = pg_fetch_array($result))
 	{
 		
-		$json .= "    " . $row[0] . ",\n"; 
+		$json .= $row[0] . ","; 
 	}
-	$json = trim($json, ",\n");
-	$json .= "\n  ],\n";
+	$json = trim($json, ",");
+	$json .= " ],";
 
 
 	$result = pg_query($con, "SELECT json_build_object('Name', name, 'Time', time) FROM (SELECT name, time FROM _Table_ WHERE difficulty = 2 ORDER BY time, id LIMIT 10) _Table_" );
 
-	$json .= '"hard": ['."\n";
+	$json .= '"hard": [';
 	while ($row = pg_fetch_array($result))
 	{
 		
-		$json .= "    " . $row[0] . ",\n"; 
+		$json .= $row[0] . ","; 
 	}
-	$json = trim($json, ",\n");
-	$json .= "\n  ]\n";
+	$json = trim($json, ",");
+	$json .= " ]";
 
 	$json .= "}}";
 
